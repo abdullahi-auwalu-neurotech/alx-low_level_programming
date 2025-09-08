6e3b37dfa2e7cf6e78ccfa3071aee611f555156e
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2025-09-08 15:01:22 +0100

    add mandatory tasks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sizes of various types
+ *
+ * Return: returns 0 if successful
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This repo contains tasks for the 0x00-hello_world project
