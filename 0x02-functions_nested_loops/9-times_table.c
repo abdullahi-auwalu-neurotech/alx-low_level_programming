a8b3d796ba339337a45cc7a3b4d8f269e6184a72
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2025-10-31 11:16:25 +0100

    debug 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,20 +18,25 @@ void times_table(void)
 			k = i * j;
 			if (k < 10)
 			{
+				if ((k < 10) && (j != 0))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar('0' + k);
 			}
 			else
 			{
+				if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 				m = k / 10;
 				n = k % 10;
 				_putchar('0' + m);
 				_putchar('0' + n);
-			}
-
-			if ((i < 10) && (j < 9))
-			{
-				_putchar(',');
-				_putchar(' ');
 			}
 			j++;
 		}
