b23d91897b6e365383cb358ff6ad7dd1892b1222
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2025-11-04 13:28:24 +0100

    debug the earlier solutions

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,9 +2,9 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -12,5 +12,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
-	return(0);
+
+	return (0);
 }
