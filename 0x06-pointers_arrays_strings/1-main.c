4bbe09696f3420c52da90c0976fde19ddbafeae5
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2025-12-03 16:59:42 +0100

    debug task 3

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -9,10 +9,9 @@
 int main(void)
 {
 	char s1[] = "Holberton";
-	char s2[] = "Holbeerton!";
 
 	/*printf("%d\n", _strcmp(s1, s2));*/
-	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
 	/*printf("%d\n", _strcmp(s1, s1));*/
 	return (0);
 }
