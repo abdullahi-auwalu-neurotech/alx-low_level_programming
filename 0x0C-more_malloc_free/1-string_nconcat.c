bfa438f6678cd3087e74d9add7c017ab11757a20
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2026-01-09 17:57:48 +0100

    debug a source file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,6 +16,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	int i, j, k;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; (s2[j] != '\0') && ((unsigned int)j != n); j++)
@@ -33,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		k++;
 	}
 	i = 0;
-	while (j >= 0)
+	while (j > 0)
 	{
 		str[k] = s2[i];
 		k++;
