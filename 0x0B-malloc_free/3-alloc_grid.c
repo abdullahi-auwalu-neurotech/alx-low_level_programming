45036c10594b6deef293511385e97f5e141c85b7
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2026-01-12 14:30:57 +0100

    debug 3*.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,10 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			free(grid);
 			return (NULL);
+		}
 		i++;
 	}
 	for (i = 0; i < height; i++)
