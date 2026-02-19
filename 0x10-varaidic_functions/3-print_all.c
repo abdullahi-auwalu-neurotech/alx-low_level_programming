7958e376bb0e5bcb44a533a18e9017c6137e4618
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2026-02-19 12:34:07 +0100

    add variadic functions tasks

---
+++ 0x10-varaidic_functions/3-print_all.c
@@ -0,0 +1,91 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i;
+
+	struct prnt p[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'0', NULL}};
+	const char *p_ptr = format;
+
+	va_start(args, format);
+	while (*p_ptr != '\0')
+	{
+		i = 0;
+		while (p[i].c != '0')
+		{
+			if (p[i].c == *p_ptr)
+			{
+				p[i].ptr(args);
+				if (*(p_ptr + 1) != '\0')
+					printf(", ");
+			}
+			i++;
+		}
+		p_ptr++;
+	}
+	va_end(args);
+	printf("\n");
+}
+
+
+/**
+ * print_string - prints strings
+ * @args: variadic argument
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		printf("(nil)");
+	printf("%s", str);
+}
+
+
+/**
+ * print_char - prints characters
+ * @args: variadic argument
+ */
+void print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	printf("%c", c);
+}
+
+
+/**
+ * print_int - prints integers
+ * @args: variadic argument
+ */
+void print_int(va_list args)
+{
+	int n = va_arg(args, int);
+
+	printf("%d", n);
+}
+
+
+/**
+ * print_float - prints float
+ * @args: variadic argument
+ */
+void print_float(va_list args)
+{
+	float f = va_arg(args, double);
+
+	printf("%f", f);
+}
+
---
+++ 0x10-varaidic_functions/README.md
@@ -0,0 +1,10 @@
+# Project: 0x10. C - Variadic functions
+
+## Description
+This project is part of alx SE program. It focuses on testing your knowledge of variadic function concepts in C programming language.
+
+## Tasks
+- 0-sum_them_all.c
+- 1-print_numbers.c
+- 2-print_strings.c
+- 3-print_all.c
